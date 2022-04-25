f46248b2fbca6c081ee69856c8d08b6d55bc4c2c
soldrh <sdelriodeheredia@gmail.com>
2022-04-25 14:00:31 -0300

    1

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -8,5 +8,37 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year_start: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
   });
 };
